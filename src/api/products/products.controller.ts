546108f18fdd24d4bc7ef3274b5aab5a9c2bf1a2
restuadil <restuadil09@gmail.com>
2025-09-19 14:27:21 +0700

    feat(product): impelement product module

--- src/api/products/products.controller.ts
+++ src/api/products/products.controller.ts
@@ -1,17 +1,8 @@
-import { Body, Controller, HttpCode, HttpStatus, Inject, Post } from "@nestjs/common";
+import { Controller, Inject } from "@nestjs/common";
 
-import { UserRole } from "@prisma/client";
 import { WINSTON_MODULE_PROVIDER } from "nest-winston";
 import { Logger } from "winston";
 
-import { Me } from "src/common/decortators/me.decorator";
-import { Roles } from "src/common/decortators/roles.decorator";
-import { ZodPipe } from "src/common/pipe/zod.pipe";
-import { UserPayload } from "src/types/jwt.type";
-import { ControllerResponse } from "src/types/web.type";
-
-import { CreateProductDto, createProductSchema } from "./dto/create-product.dto";
-import { ResponseProductDto } from "./dto/response-product.dto";
 import { ProductsService } from "./products.service";
 
 @Controller("products")
@@ -20,20 +11,4 @@ export class ProductsController {
     @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
     private readonly productsService: ProductsService,
   ) {}
-
-  @Post()
-  @HttpCode(HttpStatus.CREATED)
-  @Roles(UserRole.STORE_ADMIN)
-  async create(
-    @Me() me: UserPayload,
-    @Body(new ZodPipe(createProductSchema)) createProductDto: CreateProductDto,
-  ): Promise<ControllerResponse<ResponseProductDto>> {
-    this.logger.info("Creating product...");
-
-    const result = await this.productsService.create(me.id, createProductDto);
-    return {
-      message: "Product created successfully",
-      data: result,
-    };
-  }
 }
